70ff11280a701de7d070b546158a203f93b16971
Raffael Comi <RaffaelComi@web.de>
2022-10-23 16:09:02 +0200

    Initial adjustments

--- config/metadata.cjs
+++ config/metadata.cjs
@@ -1,26 +1,14 @@
-const {
-  author,
-  dependencies,
-  repository,
-  version,
-} = require("../package.json");
+const { name, author, repository, version } = require("../package.json");
 
 module.exports = {
   name: {
-    "": "webpack-userscript-template",
-    cn: "中文名",
-    en: "english name",
-  },
-  namespace: "https://trim21.me/",
-  version: version,
-  author: author,
+    "": name,
+    en: "Twitch Drops Manager",
+  }, // 'license': 'MIT',
+  author,
+  description: "Twitch Drops Manager",
+  match: ["https://*.twitch.tv/drops/*"],
   source: repository.url,
-  // 'license': 'MIT',
-  match: ["*://www.example.com/", "*://example.com/*"],
-  require: [
-    `https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`,
-  ],
-  grant: ["GM.xmlHttpRequest"],
-  connect: ["httpbin.org"],
-  "run-at": "document-end",
+  version,
+  require: [],
 };
